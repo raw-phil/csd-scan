1123d2df76a3fe0686651594207fad94e2210669
raw-phil <>
2024-09-30 18:32:27 +0200

    fix in cli, update README.md and Dockerfile

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # csd-scan
-Inspired by James Kettle’s research on [Browser-Powered Desync Attacks](https://portswigger.net/research/browser-powered-desync-attacks), I decided to write a simple CLI tool to automate the scanning of a web server for [Client-side desync](https://portswigger.net/research/browser-powered-desync-attacks#csd) vulnerabilities.
+Inspired by James Kettle’s research on [Browser-Powered Desync Attacks](https://portswigger.net/research/browser-powered-desync-attacks), I decided to write a simple CLI tool 
+to automate the scanning of a web server for [Client-side desync](https://portswigger.net/research/browser-powered-desync-attacks#csd) vulnerabilities.
+
+
+**csd-scan** will send a POST request to the first `URL` ( **which is the endpoit to test for vuln** ) with an incomplete GET request to `ERROR_URL` path in the body.
+
+```
+POST /FIRST_URL_PATH\r\n
+Host: vulnerable-site.com\r\n
+Content-Length: 23\r\n
+\r\n
+GET /ERROR_URL_PATH HTTP/1.1\r\n
+X: Y
+```
+
+Then it sends a GET request to the root path ('/'), and if it return the status code of the `GET /ERROR_URL_PATH HTTP/1.1` means that the server ignore Content-Length of the POST request and that the connection is successfully poisoned.
+
+```
+GET /ERROR_URL_PATH HTTP/1.1
+X: YGET / HTTP/1.1
+Host: vulnerable-site.com
+```
+
+**csd-scan** uses [node-fetch](https://www.npmjs.com/package/node-fetch) to make the test more realistic since the attack will be lauched from a web browser
+
+
+## Install
+
+Require 
+
+```bash
+git clone https://github.com/raw-phil/csd-scan.git
+cd csd-scan
+npm i
+npm run build
+npm link
+```
+
+### Docker
+
+#### Build image
+```bash
+git clone https://github.com/raw-phil/csd-scan.git
+cd csd-scan
+docker build -t csd-scan .
+```
+#### Run
+```bash
+docker run --rm csd-scan
+```
+
+## Usage
+
+```bash
+$ csd-scan --help
+
+Usage: csd-scan -u <url> -e <url>
+
+CLI tool that check if a web-server endpoint have a possible client-side desync vulnerability.
+
+Options:
+  -u, --url <url>        Possible vulnerable endpoint URL.
+  -e, --error-url <url>  Url used for inject request in the first request body.
+                         A GET request to this url MUST return an error status code ( from 400 to 599 ).
+  -h, --help             display help for command
+
+
+Examples:
+  $ cli-tool --url https://example.com --error-url https://example.com/hope404
+  $ cli-tool -u https://example.com --e https://example.com/hope404
+```
+
+
+### Arguments
+**csd-scan** needs two arguments to work:
+
+- URL passed with `-u` or `--url` flag, is the url that indicate where scan for the vulnerability
+
+- ERROR_URL passed with `-e` or `--error-url`, is the url used to inject the second request in the body of the first.
+A GET request to this url MUST return an error status code ( from 400 to 599 ). This is necessary to determine if the body of the first request affects the response of the second request, which should return a different status code than the second request to the root path.
--- cli.ts
+++ cli.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env ts-node
+#! /usr/bin/env node
 
 import fetch from "node-fetch";
 import { Command } from "commander";
@@ -11,9 +11,9 @@ import { Command } from "commander";
   program
     .name("csd-scan")
     .usage("-u <url> -e <url>")
-    .description("CLI tool to check if a web-server endpoint have a possible client-side desync vulnerability.")
+    .description("CLI tool that check if a web-server endpoint have a possible client-side desync vulnerability.")
     .option("-u, --url <url>", "Possible vulnerable endpoint URL.")
-    .option("-e, --error-url <url>", "Url used for inject request in the first request body.\nA GET request to this url MUST return a different status code than the first ulr.")
+    .option("-e, --error-url <url>", "Url used for inject request in the first request body.\nA GET request to this url MUST return an error status code ( from 400 to 599 ).")
     .on("--help", () => {
       console.log("\n\nExamples:");
       console.log("  $ cli-tool --url https://example.com --error-url https://example.com/hope404");
@@ -28,8 +28,13 @@ import { Command } from "commander";
           process.exit(1);
         }
 
+        console.log("start", cmd.url, cmd.errorUrl)
+
         const url = new URL(cmd.url);
         const errorUrl = new URL(cmd.errorUrl);
+
+        console.log("start2")
+        console.log(url)
 
         if (url.host !== errorUrl.host) {
           console.error("URL and ERROR-URL must be for the same host ");
@@ -37,27 +42,26 @@ import { Command } from "commander";
         }
 
         // Expected status code that second request have to return for being vulnerable
-        const errorUrlResponse = (await fetch(errorUrl));
+        const errorUrlResponse = (await fetch(errorUrl, { redirect: "manual" }));
 
-        const inputUrlResponse = (await fetch(url, { redirect: "manual" }));
-
-        if (errorUrlResponse.status === inputUrlResponse.status) {
-          console.error("Requests (GET) to URL and ERROR-URL have to return two different status code ");
+        if (errorUrlResponse.status <= 399) {
+          console.error("Requests (GET) to ERROR-URL must return an error code (from 400 to 599)");
           process.exit(1);
         }
 
         await fetch(url, {
           method: "POST",
           redirect: "manual",
-          body: `GET ${errorUrl.pathname + errorUrl.search} HTTP/1.1\r\nFoo: x`,
+          body: `GET ${errorUrl.pathname + errorUrl.search} HTTP/1.1\r\nX: Y`,
         });
 
-        const response = await fetch(url);
+        // Request to root path
+        const response = await fetch(url.protocol + url.host);
 
         if (response.status === errorUrlResponse.status) {
           console.log(`VULNERABLE: ${url.host} ingore request Content-Length at '${url.pathname + url.search}' path `);
-        }else{
-          console.log("Nothig found ...")
+        } else {
+          console.log("Nothing found ...")
         }
       } catch (error) {
         console.error("Error fetching the URL:", error);
